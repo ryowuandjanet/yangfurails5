c40d93246ebac7bc86dce62b70198ef2404c535f
ryowuandjanet <ryowudata@gmail.com>
2020-07-17 16:06:34 +0000

    20200718 commit

--- app/controllers/yfcases_controller.rb
+++ app/controllers/yfcases_controller.rb
@@ -2,7 +2,7 @@ class YfcasesController < ApplicationController
   include ApplicationHelper
   before_action :set_yfcase, only: [:edit, :update, :destroy, :deedtax, :yfratingscale, :realestateregistration, :complaint, :letter]
   before_action :show_helper, only: [:edit, :update, :destroy, :deedtax, :yfratingscale, :realestateregistration, :complaint, :letter]
-  before_action :authenticate_user!, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /yfcases
   # GET /yfcases.json
