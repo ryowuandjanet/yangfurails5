2128cfbcb5a00556f38db31b9c281acb117f7e4d
ryowuandjanet <ryowudata@gmail.com>
2020-07-20 09:50:32 +0000

    20200720第五次上傳

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_07_16_031100) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "builds", force: :cascade do |t|
     t.string "build_number"
     t.string "build_url"
